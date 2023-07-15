cbcef4e6a33843d76ba8e09d89c6522505dc64cb
HongyielSuh <127407241+HongyielSuh@users.noreply.github.com>
2023-07-15 13:46:01 +0900

    Update app.py

--- app.py
+++ app.py
@@ -6,42 +6,16 @@ from datetime import datetime as dt
 
 st.write("testing?")
 
-def cmpltTask(task):
-    idx = st.session_state.mytsks.index(task)
-    st.session_state.chkarr[idx] = not st.session_state.chkarr[idx]
-    st.session_state.rerun = True
 
-def listTasks():
-    st.session_state.tskclk = []
-    for i, task in enumerate(st.session_state.mytsks):
-        st.session_state.tskclk.append(st.checkbox(task, value = st.session_state.chkarr[i], key = 'l' + f'{dt.now():%d%m%Y%H%M%S%f}', on_change = cmpltTask, args=(task,), help ='Check to mark as completed'))
+title1 = st.text_input('UID', 'smcfb.01.0099')
+title2 = st.text_input('START', '2023-07-14')
+title3 = st.text_input('ACESS_TOKEN', 'eyHDSDS ...')
 
+# check box 로 바꿔서 실시간 데이터 베이스 전송 가능
+title4 = st.text_input('status', 'o OR x')
 
-st.title("Customer Database App")
+st.write('UID: ', title1)
+st.write('START: ', title2)
+st.write('ACESS_TOKEN: ', title3)
+st.write('satus: ', title4)
 
-if "mytsks" not in st.session_state:
-    st.session_state.mytsks = []
-
-if "tskclk" not in st.session_state:
-    st.session_state.tskclk = []
-
-if "chkarr" not in st.session_state:
-    st.session_state.chkarr = []
-
-if "rerun" not in st.session_state:
-    st.session_state.rerun = False
-
-
-
-if st.session_state.rerun == True:
-    st.session_state.rerun = False
-    st.experimental_rerun()
-
-else:
-    tsk = st.text_input('Enter your tasks', value="", placeholder = 'Enter a task')
-    if st.button('Add Task'):
-        if tsk != "":
-            st.session_state.mytsks.append(tsk)
-            st.session_state.chkarr.append(False)
-
-listTasks()
