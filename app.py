33e7ee0ba77ce7fa42926c7a6e248494b036d52c
HongyielSuh <127407241+HongyielSuh@users.noreply.github.com>
2023-07-15 13:40:42 +0900

    Update app.py

--- app.py
+++ app.py
@@ -17,37 +17,31 @@ def listTasks():
         st.session_state.tskclk.append(st.checkbox(task, value = st.session_state.chkarr[i], key = 'l' + f'{dt.now():%d%m%Y%H%M%S%f}', on_change = cmpltTask, args=(task,), help ='Check to mark as completed'))
 
 
-def main():
-    st.title("Customer Database App")
+st.title("Customer Database App")
 
-    if "mytsks" not in st.session_state:
-        st.session_state.mytsks = []
+if "mytsks" not in st.session_state:
+    st.session_state.mytsks = []
 
-    if "tskclk" not in st.session_state:
-        st.session_state.tskclk = []
+if "tskclk" not in st.session_state:
+    st.session_state.tskclk = []
 
-    if "chkarr" not in st.session_state:
-        st.session_state.chkarr = []
+if "chkarr" not in st.session_state:
+    st.session_state.chkarr = []
 
-    if "rerun" not in st.session_state:
-        st.session_state.rerun = False
-
+if "rerun" not in st.session_state:
+    st.session_state.rerun = False
 
 
-    if st.session_state.rerun == True:
-        st.session_state.rerun = False
-        st.experimental_rerun()
 
-    else:
-        tsk = st.text_input('Enter your tasks', value="", placeholder = 'Enter a task')
-        if st.button('Add Task'):
-            if tsk != "":
-                st.session_state.mytsks.append(tsk)
-                st.session_state.chkarr.append(False)
+if st.session_state.rerun == True:
+    st.session_state.rerun = False
+    st.experimental_rerun()
 
-    
+else:
+    tsk = st.text_input('Enter your tasks', value="", placeholder = 'Enter a task')
+    if st.button('Add Task'):
+        if tsk != "":
+            st.session_state.mytsks.append(tsk)
+            st.session_state.chkarr.append(False)
 
-
-if __name__ == '__main__':
-    main()
-    listTasks()
+listTasks()
