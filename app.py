9d1feeb313314f718223e877e0b6c83e9c99d96f
HongyielSuh <127407241+HongyielSuh@users.noreply.github.com>
2023-07-15 14:00:00 +0900

    Update app.py

--- app.py
+++ app.py
@@ -7,32 +7,32 @@ import pymysql
 st.write("testing?")
 
 
-UID = st.text_input('UID', 'smcfb.01.0099')
-START = st.text_input('START', '2023-07-14')
-ACCESS_TOKEN = st.text_input('ACESS_TOKEN', 'eyHDSDS ...')
 
-# check box 로 바꿔서 실시간 데이터 베이스 전송 가능
-status = st.text_input('status', 'o OR x')
 
-st.write('UID: ', UID)
-st.write('START: ', START)
-st.write('ACESS_TOKEN: ', ACCESS_TOKEN)
-st.write('satus: ', status)
+
 
 
 def on_button_click():
     st.session_state.error_message = ''
     st.session_state.result_message = ''
-    if not str(st.session_state.user_name):
-        st.session_state.error_message = "Input your name please~"
+    if not str(st.session_state.UID) and not str(st.session_state.START) and not str(st.session_state.ACESS_TOKEN) and not str(st.session_state.status):
+        st.session_state.error_message = "포멧을 정확히 입력해주세요"
     else:
-        st.session_state.result_message = f"Hello~ {str(st.session_state.user_name)}"
+        st.session_state.result_message = f"DB was updated {str(st.session_state.UID)}"
 
 
 st.title("Streamlit Test")
 
-input_user_name = st.text_input(label="User Name", key='user_name', value="")
+input_user_name = st.text_input(key = 'UID', value = '')
+input_start_date = st.text_input(key = 'START', value = '2023-07-14')
+input_access_token = st.text_input(key = 'ACESS_TOKEN', value = 'eyHDSDS ...')
+# check box 로 바꿔서 실시간 데이터 베이스 전송 가능
+status = st.text_input('status', 'o OR x')
 
+st.write('UID: ', input_user_name)
+st.write('START: ', input_start_date)
+st.write('ACESS_TOKEN: ', input_access_token)
+st.write('satus: ', status)
 
 checkbox = st.checkbox('agree')
 st.button("Confirm", key='confirm_btn', disabled=(checkbox is False), on_click=on_button_click)
