b082aa0320641731ac2ef8198192eb49a1f57528
HongyielSuh <127407241+HongyielSuh@users.noreply.github.com>
2023-07-20 15:31:35 +0900

    Update app.py

--- app.py
+++ app.py
@@ -51,8 +51,8 @@ def page_home():
 
 
     # 체크 박스를 통해 시그널 전송 (상태 전송)
-    checkbox = st.checkbox('버튼잠금풀기')
-    confirm_btn = st.button("Confirm", key='confirm_btn', disabled=(checkbox is False), on_click=on_button_click)
+    # checkbox = st.checkbox('버튼잠금풀기')
+    # confirm_btn = st.button("Confirm", key='confirm_btn', disabled=(checkbox is False), on_click=on_button_click)
 
     con = st.container()
     con.caption("Result")
