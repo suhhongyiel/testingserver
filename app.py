e37baca4d30a008c946144d370585dde31bcc9ea
HongyielSuh <127407241+HongyielSuh@users.noreply.github.com>
2023-07-20 13:52:11 +0900

    Update app.py

--- app.py
+++ app.py
@@ -89,9 +89,8 @@ def page_about():
     st.info(f"Selected Device Info: {device_info}")
     st.info(f"Table Name: {table_name}")
     all_name = get_table_names()
-    for i in all_name:
-        if i in table_name:
-            st.write(i)
+    # for i in all_name:
+    #     f{all_name}
 
 # dbeaver 에서 테이블 이름들 가져오기
 def get_table_names():
