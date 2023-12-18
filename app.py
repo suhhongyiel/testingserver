288aef53d5f56918ae985f8958c3dd4e02c7d639
HongyielSuh <127407241+HongyielSuh@users.noreply.github.com>
2023-12-18 20:45:29 +0900

    Update app.py

--- app.py
+++ app.py
@@ -325,26 +325,41 @@ def page_download():
     )
 
 # dbeaver 에서 해당되는 데이터 테이블 가져오기
-def get_table_names(table_name):
-    table_names = []
-    st.write("here is table name: ", table_name)
+# def get_table_names(table_name):
+#     table_names = []
+#     st.write("here is table name: ", table_name)
+#     try:
+#         with db.cursor() as cursor:
+#             query = "SHOW TABLES LIKE %s"
+#             like_pattern = f"%{table_name}%"  # LIKE 패턴 생성
+#             st.write("Executing query: ", query % like_pattern)  # 쿼리 로그 출력
+#             cursor.execute(query, (like_pattern,))
+#             result = cursor.fetchall()
+#             for row in result:
+#                 table_names.append(row[0])
+
+#             st.write("this is table names: ", result)
+
+#     except pymysql.Error as e:
+#         st.error(f"An error occurred: {e}")  # Streamlit 에러 메시지 출력
+
+#     return table_names
+def get_all_tables(table_name):
     try:
         with db.cursor() as cursor:
-            query = "SHOW TABLES LIKE %s"
-            like_pattern = f"%{table_name}%"  # LIKE 패턴 생성
-            st.write("Executing query: ", query % like_pattern)  # 쿼리 로그 출력
-            cursor.execute(query, (like_pattern,))
+            # 모든 테이블 이름 가져오기
+            query = "SHOW TABLES"
+            cursor.execute(query)
             result = cursor.fetchall()
-            for row in result:
-                table_names.append(row[0])
-
-            st.write("this is table names: ", result)
+            
+            # 결과에서 테이블 이름 추출
+            table_names = [row[0] for row in result]
+            return table_names
 
     except pymysql.Error as e:
-        st.error(f"An error occurred: {e}")  # Streamlit 에러 메시지 출력
-
-    return table_names
-
+        print(f"An error occurred: {e}")
+        return []
+    
 # dbeaver 에서 해당하는 데이터 Columns 가져오기
 def get_table_data(smcfb_info):
     data = []
